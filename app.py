aa6f2d56d5c393d62e83286448ba2a0a1a37a4a6
maravenag <aravenagamboa@gmail.com>
2017-08-18 15:24:11 -0300

    hot fixs

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ def send_response(msg_type, text):
 def handle_response(text, channel, user):
 
     if ("ayudame") in text:
-        send_response(msg_type="message", text="¿Aún no conoces los comandos y opciones? No hay problema, aquí están:/n- Crea Evaluación/n- Modifica Evaluación")
+        send_response(msg_type="message", text="¿Aún no conoces los comandos y opciones? No hay problema, aquí están:\n- Crea Evaluación\n- Modifica Evaluación")
         
     if ("a q hora tomamos") in text:
        send_response(msg_type="message", text="A las 22:00hrs en el bunker")
